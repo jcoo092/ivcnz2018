096bcfa95173e3393782776e1c3a461a95a067d1
James Cooper <jcoo092@UoA.auckland.ac.nz>
2018-10-25 19:06:08 +1300

    Borrowed benchmarking to generate the images for comparison.

--- Benchmarking/Program.fs
+++ Benchmarking/Program.fs
@@ -17,6 +17,8 @@ open BenchmarkDotNet.Exporters
 open BenchmarkDotNet.Exporters.Csv
 open BenchmarkDotNet.Columns
 open BenchmarkDotNet.Reports
+open SixLabors.ImageSharp.Formats.Png
+open System.IO
 
 // Much of this is basically copied/adapted from https://github.com/fsharp/fsharp/blob/master/tests/scripts/array-perf/array-perf.fs
 
@@ -78,7 +80,52 @@ let main argv =
 
     Configuration.Default.MemoryAllocator <- ArrayPoolMemoryAllocator.CreateWithModeratePooling()
 
-    let switch = BenchmarkSwitcher [|typeof<PerfBenchmark>|]
+    //let switch = BenchmarkSwitcher [|typeof<PerfBenchmark>|]
 
-    switch.Run argv |> ignore
+    //switch.Run argv |> ignore
+
+    let base_filenames = ["very small"; "small"; "medium"; "peppers_gray"]
+    let window_sizes = [3; 5; 7; 9; 11]
+    let inputdir = @"..\Images\Inputs\"
+    let outputdirbase = @"..\Images\Outputs\"
+
+    for fn in base_filenames do
+        (* use input_image = Image.Load(inputdir + fn + ".png")
+        input_image.Mutate(fun x -> x.Grayscale() |> ignore)
+        use out_file = new System.IO.FileStream(outputdirbase + @"\gray\" + fn + "_gray.png", FileMode.OpenOrCreate) *)
+        let pngenc = PngEncoder()
+        pngenc.ColorType <- PngColorType.Rgb
+        (* input_image.Save(out_file, pngenc)
+        let intensities = img.GetPixelSpan().ToArray() |> Array.map (fun p -> p.R)
+        let imgWidth = input_image.Width
+        let imgHeight = input_image.Height *)
+
+        use input_image = Image.Load(inputdir + fn + "_noisy.png")
+        input_image.Mutate(fun x -> x.Grayscale() |> ignore)
+        let intensities = input_image.GetPixelSpan().ToArray() |> Array.map (fun p -> p.R)
+        let imgWidth = input_image.Width
+        let imgHeight = input_image.Height
+
+        for ws in window_sizes do
+            use naive_file = new System.IO.FileStream(outputdirbase + @"\Naive\" + fn + "_naive_" + string ws + ".png", FileMode.OpenOrCreate)
+            use out_img = Naive.medianFilter intensities imgWidth imgHeight ws
+            out_img.Save(naive_file, pngenc)
+            naive_file.Dispose()
+            out_img.Dispose()
+
+            use braunl_file = new System.IO.FileStream(outputdirbase + @"\Braunl\" + fn + "_braunl_" + string ws + ".png", FileMode.OpenOrCreate)
+            use out_img = Braunl.medianFilter intensities imgWidth imgHeight ws
+            out_img.Save(braunl_file, pngenc)
+            braunl_file.Dispose()
+            out_img.Dispose()
+
+            use cml_file = new System.IO.FileStream(outputdirbase + @"\CML\" + fn + "_cml_" + string ws + ".png", FileMode.OpenOrCreate)
+            use out_img = CML.medianFilter intensities imgWidth imgHeight ws
+            out_img.Save(cml_file, pngenc)
+            cml_file.Dispose()
+            out_img.Dispose()
+
+        input_image.Dispose()
+
+
     0 // return an integer exit code
